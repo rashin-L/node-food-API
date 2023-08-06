e6f256836a7ce85ade37e8ce51b28229bc8188ab
Rashin <134249362+rashin-L@users.noreply.github.com>
2023-08-05 22:21:53 -0700

    Add app

---
+++ app/http/middleware/RestaurantAdmin.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+module.exports = function (req, res, next) {
+  try{
+    const role = req.user.role
+    if (role==="restaurant")
+    return next()
+    else 
+    return res.status(401).send('شما اجازه دسترسی به این دیتا را ندارید');
+  }catch(err){
+    console.log(err);
+    return res.status(401).send('شما اجازه دسترسی به این دیتا را ندارید');
+  }
+};
---
+++ app/http/validator/RestaurantValidator.js
@@ -0,0 +1,45 @@
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
+
+const validateCreateRestaurant = (data) => {
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    description: Joi.string().required(),
+    address: Joi.string(),
+    adminUsername: Joi.string().required(),
+    adminPassword: Joi.string().required(),
+  });
+  return schema.validate(data);
+};
+
+const validateUpdateRestaurant = (data) => {
+  const schema = Joi.object({
+    name: Joi.string(),
+    description: Joi.string(),
+    address: Joi.string(),
+    adminUsername: Joi.string(),
+    adminPassword: Joi.string(),
+  });
+  return schema.validate(data);
+};
+
+const loginValidator = (data) => {
+  const schema = Joi.object({
+    username: Joi.string().required(),
+    password: Joi.string().required(),
+  });
+  return schema.validate(data);
+};
+
+
+const foodValidator = (data) => {
+  const schema = Joi.object({
+    name:Joi.string().required(),
+    description:Joi.string().required(),
+    price:Joi.number().required()
+  });
+  return schema.validate(data);
+};
+
+
+module.exports = { validateCreateRestaurant,validateUpdateRestaurant,loginValidator,foodValidator };
